bd8a42411d8cef9a4b60ad268292410d000e710d
Rondy Sousa <rondy.sousa@plataformatec.com.br>
2016-09-09 18:49:31 -0300

    Avoid rescuing `Exception` on FetchContentFromWebPage

--- app/models/fetch_content_from_web_page.rb
+++ app/models/fetch_content_from_web_page.rb
@@ -2,7 +2,7 @@ class FetchContentFromWebPage
   def call(action:, on_success:, on_error:)
     response = action.call
     on_success.call(response)
-  rescue Exception => exception
+  rescue => exception
     error_message = build_error_message_from(exception)
     on_error.call(error_message)
   end
