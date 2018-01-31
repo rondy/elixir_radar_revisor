6a4b8e7a33bd47e0d5c4622a974f9d10c0eb3b9f
Fabio Albini <fabio.albini@plataformatec.com.br>
2018-01-31 10:26:36 -0200

    Uses current events in the tests

--- spec/models/revise_events_spec.rb
+++ spec/models/revise_events_spec.rb
@@ -5,14 +5,14 @@ describe ReviseEvents do
     consistent_entry = {
       title: 'ElixirConf',
       url: 'http://elixirconf.com/',
-      subtitle: 'Conference = (DOLAR)450; Conference + Training = (DOLAR)700. (expiration: August 22)\n            <br>',
+      description: 'ElixirConf™ US - Bellevue, WA, September 5-8, 2017.',
       tag: 'event'
     }
 
     divergent_entry = {
       title: 'EmpEx -- Halloween Lightning Talks 2016',
       url: 'http://empex.co/',
-      subtitle: 'Call for Proposals now open\n        <br>',
+      description: 'Call for Proposals now open\n        <br>',
       tag: 'event'
     }
 
@@ -31,149 +31,41 @@ describe ReviseEvents do
     expect(divergent_result_entry[:divergences]).to be_present
     expect(divergent_result_entry[:divergences].first[:reason]).to eq('page_title_does_not_match')
     expect(divergent_result_entry[:divergences].first[:details][:given_page_title]).to eq('EmpEx -- Halloween Lightning Talks 2016')
-    expect(divergent_result_entry[:divergences].first[:details][:fetched_page_title]).to eq('Empire City Elixir Conf')
+    expect(divergent_result_entry[:divergences].first[:details][:fetched_page_title]).to eq('EMPEX')
   end
 
   context 'when events are from Meetup.com' do
     it 'revises a event entry with consistent title' do
       entry = {
-        title: 'Sarasota, FL',
-        url: 'https://www.meetup.com/SarasotaSoftwareEngineers/events/232976666/',
-        subtitle: 'Concurrent Programming with the Elixir ecosystem',
-        description: 'Wednesday, August 31, 2016',
+        title: "Let's talk Elixir",
+        url: 'https://www.meetup.com/Baltimore-Elixir-and-Erlang-Meetup/events/246844011/',
+        description: 'Wednesday, January 31, 2018 - Baltimore, MD, USA',
         tag: 'event'
       }
 
       revision_result = ReviseEvents.new.call([entry])
 
       consistent_result_entry = revision_result.first
-      expect(consistent_result_entry[:entry_title]).to eq('Sarasota, FL')
+      expect(consistent_result_entry[:entry_title]).to eq("Let's talk Elixir")
       expect(consistent_result_entry[:divergences]).to be_empty
     end
 
     it 'revises a event entry with divergent title' do
       entry = {
-        title: 'Indianapolis­, IN',
-        url: 'http://www.meetup.com/indyelixir/events/233392329/',
-        subtitle: 'Releasing Hex packages and neural networks',
-        description: 'Tuesday, September 6, 2016',
+        title: 'Elixir PoA Tech Talks',
+        url: 'https://www.meetup.com/Baltimore-Elixir-and-Erlang-Meetup/events/246844011/',
+        description: 'Wednesday, January 31, 2018 - Baltimore, MD, USA',
         tag: 'event'
       }
 
       revision_result = ReviseEvents.new.call([entry])
 
       divergent_result_entry = revision_result.first
-      expect(divergent_result_entry[:entry_title]).to eq('Indianapolis­, IN')
+      expect(divergent_result_entry[:entry_title]).to eq('Elixir PoA Tech Talks')
       expect(divergent_result_entry[:divergences]).to be_present
       expect(divergent_result_entry[:divergences].first[:reason]).to eq('event_title_does_not_match')
-      expect(divergent_result_entry[:divergences].first[:details][:given_event_title]).to eq('Releasing Hex packages and neural networks')
-      expect(divergent_result_entry[:divergences].first[:details][:fetched_event_title]).to eq('Indy Elixir Monthly Meetup')
-    end
-
-    it 'revises an event entry with consistent event date (past event)' do
-      entry = {
-        title: 'Tucson, AZ',
-        url: 'https://www.meetup.com/Tucson-Elixir-Meetup/events/233396783/',
-        subtitle: 'Nerves - Embedded Elixir, ElixirConf Roundup',
-        description: 'Wednesday, September 7, 2016',
-        tag: 'event'
-      }
-
-      revision_result = ReviseEvents.new.call([entry])
-
-      divergent_result_entry = revision_result.first
-      expect(divergent_result_entry[:entry_title]).to eq('Tucson, AZ')
-      # TODO: Should we also assert how many divergences have been captured?
-      expect(divergent_result_entry[:divergences]).to be_empty
-    end
-
-    it 'revises an event entry with divergent event date (past event)' do
-      entry = {
-        title: 'Tucson, AZ',
-        url: 'https://www.meetup.com/Tucson-Elixir-Meetup/events/233396783/',
-        subtitle: 'Nerves - Embedded Elixir, ElixirConf Roundup',
-        description: 'Wednesday, September 6, 2016',
-        tag: 'event'
-      }
-
-      revision_result = ReviseEvents.new.call([entry])
-
-      divergent_result_entry = revision_result.first
-      expect(divergent_result_entry[:entry_title]).to eq('Tucson, AZ')
-      expect(divergent_result_entry[:divergences]).to be_present
-      expect(divergent_result_entry[:divergences].first[:reason]).to eq('event_date_does_not_match')
-      expect(divergent_result_entry[:divergences].first[:details][:given_event_date]).to eq('Wednesday, September 6, 2016')
-      expect(divergent_result_entry[:divergences].first[:details][:fetched_event_date]).to eq('September 7 · 7:00 PM')
-    end
-
-    it 'revises an event entry with consistent event date (few days ago)' do
-      entry = {
-        title: 'Lisbon, Portugal',
-        url: 'http://www.meetup.com/lisbon-elixir/events/232868915/',
-        subtitle: 'How are people using Elixir in their jobs?',
-        description: 'Friday, September 16, 2016',
-        tag: 'event'
-      }
-
-      revision_result = ReviseEvents.new.call([entry])
-
-      divergent_result_entry = revision_result.first
-      expect(divergent_result_entry[:entry_title]).to eq('Lisbon, Portugal')
-      expect(divergent_result_entry[:divergences]).to be_empty
-    end
-
-    it 'revises an event entry with divergent event date (few days ago)' do
-      entry = {
-        title: 'Lisbon, Portugal',
-        url: 'http://www.meetup.com/lisbon-elixir/events/232868915/',
-        subtitle: 'How are people using Elixir in their jobs?',
-        description: 'Friday, September 20, 2016',
-        tag: 'event'
-      }
-
-      revision_result = ReviseEvents.new.call([entry])
-
-      divergent_result_entry = revision_result.first
-      expect(divergent_result_entry[:entry_title]).to eq('Lisbon, Portugal')
-      expect(divergent_result_entry[:divergences]).to be_present
-      expect(divergent_result_entry[:divergences].first[:reason]).to eq('event_date_does_not_match')
-      expect(divergent_result_entry[:divergences].first[:details][:given_event_date]).to eq('Friday, September 20, 2016')
-      expect(divergent_result_entry[:divergences].first[:details][:fetched_event_date]).to eq('4 days ago · 7:00 PM')
-    end
-
-    it 'revises an event entry with consistent event date (future event)' do
-      entry = {
-        title: 'Nashville, TN',
-        url: 'https://www.meetup.com/nashville-software-beginners/events/233710900/',
-        subtitle: 'Intro to Elixir and Phoenix',
-        description: 'Thursday, September 22, 2016',
-        tag: 'event'
-      }
-
-      revision_result = ReviseEvents.new.call([entry])
-
-      divergent_result_entry = revision_result.first
-      expect(divergent_result_entry[:entry_title]).to eq('Nashville, TN')
-      expect(divergent_result_entry[:divergences]).to be_empty
-    end
-
-    it 'revises an event entry with divergent event date (future event)' do
-      entry = {
-        title: 'Nashville, TN',
-        url: 'https://www.meetup.com/nashville-software-beginners/events/233710900/',
-        subtitle: 'Intro to Elixir and Phoenix',
-        description: 'Thursday, September 21, 2016',
-        tag: 'event'
-      }
-
-      revision_result = ReviseEvents.new.call([entry])
-
-      divergent_result_entry = revision_result.first
-      expect(divergent_result_entry[:entry_title]).to eq('Nashville, TN')
-      expect(divergent_result_entry[:divergences]).to be_present
-      expect(divergent_result_entry[:divergences].first[:reason]).to eq('event_date_does_not_match')
-      expect(divergent_result_entry[:divergences].first[:details][:given_event_date]).to eq('Thursday, September 21, 2016')
-      expect(divergent_result_entry[:divergences].first[:details][:fetched_event_date]).to eq('Thursday, September 22, 2016 6:00 PM')
+      expect(divergent_result_entry[:divergences].first[:details][:given_event_title]).to eq('Elixir PoA Tech Talks')
+      expect(divergent_result_entry[:divergences].first[:details][:fetched_event_title]).to eq("Let's talk Elixir")
     end
   end
 
