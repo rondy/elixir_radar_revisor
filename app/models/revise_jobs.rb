9747dee6f01c20ff6b947d7798b4b88602d41dee
Fabio Albini <fabio.albini@plataformatec.com.br>
2018-01-31 14:37:50 -0200

    Runs jobs revisions in sequence instead of using the Parallel gem
    Fixes return for jobs revisions test

--- app/models/revise_jobs.rb
+++ app/models/revise_jobs.rb
@@ -1,51 +1,56 @@
 class ReviseJobs
   def call(entries)
-    Parallel.map(
-      entries.select { |n| n[:tag]=='job' },
-      in_processes: 4
-    ) do |entry|
-      given_entry_title = entry[:title]
-
-      result_entry = {
-        entry_title: given_entry_title,
-        divergences: []
-      }
-
-      fetching_content_from_web_page(
-        action: lambda do
-          fetch_job_page_content(entry)
-        end,
-
-        on_success: lambda do |job_page_content|
-          job_entry_details = entry[:subtitle].split('-').first.strip
-
-          job_details_matches = !!(job_page_content =~ Regexp.new(Regexp.escape(job_entry_details)))
-
-          unless job_details_matches
-            result_entry[:divergences] << {
-              reason: 'job_details_does_not_match',
-              details: {
-                given_job_details: job_entry_details
-              }
-            }
-          end
-        end,
-
-        on_error: lambda do |error_message|
-          result_entry[:divergences] << {
-            reason: 'connection_error',
-            details: {
-              error_message: error_message
-            }
-          }
-        end
-      )
-
-      result_entry
+    filtered_entries(entries).map do |entry|
+      revise_entry(entry)
     end
   end
 
   private
+
+  def filtered_entries(entries)
+    entries.select { |entry| entry[:tag] == 'job' }
+  end
+
+  def revise_entry(entry)
+    given_entry_title = entry[:title]
+
+    result_entry = {
+      entry_title: given_entry_title,
+      divergences: []
+    }
+
+    fetching_content_from_web_page(
+      action: lambda do
+        fetch_job_page_content(entry)
+      end,
+
+      on_success: lambda do |job_page_content|
+        job_entry_details = entry[:subtitle].split('-').first.strip
+
+        job_details_matches = !!(job_page_content =~ Regexp.new(Regexp.escape(job_entry_details)))
+
+        unless job_details_matches
+          result_entry[:divergences] << {
+            reason: 'job_details_does_not_match',
+            details: {
+              given_job_details: job_entry_details
+            }
+          }
+        end
+      end,
+
+      on_error: lambda do |error_message|
+        result_entry[:divergences] << {
+          reason: 'connection_error',
+          details: {
+            error_message: error_message
+          }
+        }
+      end
+    )
+
+    result_entry
+  end
 
   def fetch_job_page_content(entry)
     agent = Mechanize.new
